374b726d54816a3a96a5d19fbc36c6ff3e3cd56c
noyahafiz <noyazaina@gmail.com>
2020-11-05 22:10:16 +0000

    port

--- main.py
+++ main.py
@@ -40,6 +40,6 @@ def flask():
 
 if __name__ == "__main__":
     #runs the application on the repl development server
-    app.run(debug=True, port='5000', host='127.0.0.1')
+    app.run(debug=True, port='5000', host='0.0.0.0')
     #KK
     #Karam
